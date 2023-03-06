a3d94b3083ce528df02b007b599e86cc08003f36
artin4n <julian.mjm.300@gmail.com>
2023-03-06 12:37:04 -0800

    cleaned up input.c and input.h

--- include/input.h
+++ include/input.h
@@ -1,18 +1,19 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-#include "raylib.h"
-
-typedef struct { // Keeps track of all held inputs to compare to relevant values
+// Keeps track of all relevant inputs to update game state.
+typedef struct { 
     int total_keys;
     int key_count;
     int keys_down[4];
 } InputState;
 
+// Creates a default input state. Returns an InputState struct.
 InputState initial_input_state();
 
 void add_to_input_state(InputState* input_state, int key);
 
+// Polls for current inputs.
 InputState capture_input();
 
 #endif
--- src/input.c
+++ src/input.c
@@ -1,35 +1,51 @@
 #include "../include/raylib.h"
 #include "../include/input.h"
 
-//----- INITIAL INPUT STATE SYSTEM -----
+
 InputState initial_input_state() {
+
     InputState input_state;
+
+
+    //----------------------------------
+
 
     input_state.key_count = 0;
     input_state.total_keys = 4; // We care about 4 keys: up and down for paddle 1 and paddle 2
 
-    // KEY_NULL essentially represents nothing, is used as the default key stored
+
+    //----------------------------------
+
+    
     input_state.keys_down[0] = KEY_NULL;
     input_state.keys_down[1] = KEY_NULL;
     input_state.keys_down[2] = KEY_NULL;
-    input_state.keys_down[3] = KEY_NULL;
+    input_state.keys_down[3] = KEY_NULL; // KEY_NULL represents no key, is used as the default key stored
+
+
+    //----------------------------------
+
 
     return input_state;
+
 }
-//--------------------------------------
 
 
-//----------------- ADD KEY TO INPUT STATE -----------------
 void add_to_input_state(InputState* input_state, int key) {
+
     input_state->keys_down[input_state->key_count] = key; // Add the key to the current available index
-    input_state->key_count++; // Update index counter
+    input_state->key_count++; // Update current available index counter
+
 }
-//----------------------------------------------------------
 
 
-//-------------------------- INPUT CAPTURE SYSTEM --------------------------
 InputState capture_input() {
+
     InputState input_state = initial_input_state();
+
+
+    //------------------------------------------------------------
+
 
     if (IsKeyDown(KEY_S)) add_to_input_state(&input_state, KEY_S); // paddle1 down
     if (IsKeyDown(KEY_W)) add_to_input_state(&input_state, KEY_W); // paddle1 up
@@ -37,6 +53,10 @@ InputState capture_input() {
     if (IsKeyDown(KEY_K)) add_to_input_state(&input_state, KEY_K); // paddle2 down
     if (IsKeyDown(KEY_I)) add_to_input_state(&input_state, KEY_I); // paddle2 up
 
+
+    //------------------------------------------------------------
+
+
     return input_state;
+
 }
-//--------------------------------------------------------------------------
