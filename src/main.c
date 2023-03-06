3b4cd97d474453c4d77cd6a399387da473b1c4c0
artin4n <julian.mjm.300@gmail.com>
2023-03-06 12:43:34 -0800

    cleaned up main function

--- src/main.c
+++ src/main.c
@@ -1,17 +1,11 @@
 // <3 Made with raylib
 #include "../include/raylib.h"
-// <3
 
 #include "../include/game.h"
 
-//-- SCREEN DIMENSIONS --
-#define SCREEN_WIDTH 1600
-#define SCREEN_HEIGHT 800
-//-----------------------
 
-
-//------ GAME STRUCTS ------
 int main(void) {
+
     SetConfigFlags(FLAG_VSYNC_HINT); // Turns on v-sync
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "classic_pong");
@@ -20,19 +14,34 @@ int main(void) {
 
     Game game = initial_game_state();
 
-    while (!game.close && !WindowShouldClose()) {
+
+    //------------------------------------------------------
+
+
+    while (!game.close && !WindowShouldClose()) { // Main game loop
+        
         float dt = GetFrameTime();
 
         game.input_state = capture_input();
+
+
+        //------------------------------------------------------
+
 
         handle_input(&game, dt);
 
         step_physics(&game, dt);
 
         draw_game(&game);
+
     }
+
+
+    //------------------------------------------------------
+
 
     CloseWindow();
 
     return 0;
+
 }
